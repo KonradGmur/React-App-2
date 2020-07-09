275624aacda119bd983718968e989b2833d35cb1
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-09 19:11:21 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,17 @@ class App extends Component {
   state = {
     words: []
   }
-  
+
+  componentDidMount() {
+    fetch('data/words.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          words: data.words
+        })
+      })
+  }
+
   render() {
     const words = this.state.words.map(word => (
       <Word key={word.id} english={word.en} polish={word.pl} />
