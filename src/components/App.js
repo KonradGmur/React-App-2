7fdbcd33328bc6bef3a894b6381dc8f16c31ded0
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-09 15:07:03 +0200

    update files

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+// import Word from './Word';
+
+class App extends Component {
+  state = {}
+  render() {
+    return (
+      <div>
+        App fetch
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
